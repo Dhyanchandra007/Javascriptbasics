0d7ef74a64c29c08cf8301b03e62d975956cd06d
Dhyanchandra007 <dhyanchandra400@gmail.com>
2025-07-21 16:48:14 +0000

    basic js scope like local global block .

---
+++ javascript-basics/basics-01.js
@@ -0,0 +1,9 @@
+const arr=[1,2,3,4,5,6,7];
+//console.log(arr);
+
+// global scope ,block scope,localscope
+
+console.log(a);
+//let a=20; can not access 'a' before initilization
+//var a=20;  undefined
+const a=20;  // can not access 'a' before initilization
